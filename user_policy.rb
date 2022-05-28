b72450a4b1c85dba6de7b9aa775def370f90671c
hogucc <uku.h1r8@gmail.com>
2022-05-28 21:35:28 +0900

    UserPolicyの各処理をメソッド化して処理にわかりやすい名前をつけた

--- user_policy.rb
+++ user_policy.rb
@@ -4,14 +4,30 @@ class UserPolicy
   end
 
   def administrator_account_name?
-    user.sign_in_count > 0 && user.role == "admin"
+    user_signed_in? && user_is_administrator?
   end
 
   def use_email_as_name?
-    user.full_name.blank? && user.email.present?
+    user_does_not_have_full_name? && user_has_email?
   end
 
   private
 
     attr_reader :user
+
+  def user_does_not_have_full_name?
+    user.full_name.blank?
+  end
+
+  def user_has_email?
+    user.email.present?
+  end
+
+  def user_signed_in?
+    user.sign_in_count > 0
+  end
+
+  def user_is_administrator?
+    user.role == "admin"
+  end
 end
